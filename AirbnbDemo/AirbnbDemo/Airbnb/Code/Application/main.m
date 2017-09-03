6efda9117ee9092af1d3dfa7b7386c63933f3a99
Ossey <xiaoyuan1314@me.com>
2017-09-03 14:40:12 +0800

    Initializer project

---
+++ AirbnbDemo/AirbnbDemo/Airbnb/Code/Application/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AirbnbDemo
+//
+//  Created by Swae on 03/09/2017.
+//  Copyright © 2017 Ossey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
